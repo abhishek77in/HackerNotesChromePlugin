1d3c3d8e56b5f641cffb0fab36fda45c8b545ab0
abhishek srivastava <abhishek77in@gmail.com>
2013-05-13 21:06:26 +0530

    Fisrt commit

---
+++ background.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+  // If the letter 'g' is found in the tab's URL...
+  if (tab.url.indexOf('github.com') > -1) {
+    // ... show the page action.
+    chrome.pageAction.show(tabId);
+	path = tab.url.split('//');
+	uname = path[1].split('/')[1];
+	repo = path[1].split('/')[2];
+	//alert(repo);
+	hurl = "http://www.thehackernotes.com/" + uname + '/' + repo;
+	chrome.tabs.create({url: hurl});
+  }
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
