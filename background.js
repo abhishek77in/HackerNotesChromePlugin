10333c9aa31ab4f942e034d3322a0ae66bc46895
abhishek srivastava <abhishek77in@gmail.com>
2013-05-13 21:26:06 +0530

    Remove bootstrap content

--- background.js
+++ background.js
@@ -1,10 +1,6 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
-  // If the letter 'g' is found in the tab's URL...
+  // If the 'github.com' is found in the tab's URL...
   if (tab.url.indexOf('github.com') > -1) {
     // ... show the page action.
     chrome.pageAction.show(tabId);
