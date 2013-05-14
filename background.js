d88c1dda813ee8ccc356be00f92f17a912e8c1b4
abhishek srivastava <abhishek77in@gmail.com>
2013-05-14 05:45:28 +0530

    Open hackernotes page in newtab

--- background.js
+++ background.js
@@ -1,17 +1,18 @@
-// Called when the url of a tab changes.
+// If the 'github.com' is found show icon of the extension
 function checkForValidUrl(tabId, changeInfo, tab) {
-  // If the 'github.com' is found in the tab's URL...
   if (tab.url.indexOf('github.com') > -1) {
-    // ... show the page action.
     chrome.pageAction.show(tabId);
-	path = tab.url.split('//');
-	uname = path[1].split('/')[1];
-	repo = path[1].split('/')[2];
-	//alert(repo);
-	hurl = "http://www.thehackernotes.com/" + uname + '/' + repo;
-	chrome.tabs.create({url: hurl});
   }
-};
-
-// Listen for any changes to the URL of any tab.
+}
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+// On click - open corresponding hackernotes page in new tab
+chrome.browserAction.onClicked.addListener(function (tab) {
+  path = tab.url.split('//');
+  username = path[1].split('/')[1];
+  repo = path[1].split('/')[2];
+  hackernotesUrl = "http://www.thehackernotes.com/" + username + '/' + repo;
+  chrome.tabs.create({
+    url: hackernotesUrl
+  });
+});
